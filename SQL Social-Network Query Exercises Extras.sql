ac7693573b177f8ee14f3571b14387c03dd9eaa0
AriaPhantom <AriaPhantom@users.noreply.github.com>
2018-06-08 20:30:36 -0400

    Exercise 4

--- SQL Social-Network Query Exercises Extras.sql
+++ SQL Social-Network Query Exercises Extras.sql
@@ -1,5 +1,3 @@
-use demo2;
-
 /*For every situation where student A likes student B, but student B likes a different student C, return the names 
 and grades of A, B, and C. */
 select h1.name as name1,h1.grade as grade1,h2.name as name2,h2.grade as grade2,h3.name as name3,h3.grade as grade3
@@ -29,3 +27,26 @@ from(
 	select id,0
 	from highschooler
 	where id not in (select id1 from friend)) as temp;
+    
+/*Find the number of students who are either friends with Cassandra or are friends of friends of 
+Cassandra. Do not count Cassandra, even though technically she is a friend of a friend. */
+select count(id)
+from (
+	select id2 as id
+	from (
+		select f1.id2,f2.id2 as id3
+		from friend f1,friend f2
+		where f1.ID2=f2.ID1 and f1.id1 in (select id from Highschooler where name='Cassandra') and f2.id2 not in (select id from Highschooler where name='Cassandra') ) as temp1
+	union
+	select id3 as id
+	from (
+		select f1.id2,f2.id2 as id3
+		from friend f1,friend f2
+		where f1.ID2=f2.ID1 and f1.id1 in (select id from Highschooler where name='Cassandra') and f2.id2 not in (select id from Highschooler where name='Cassandra') ) as temp2) as temp;
+        
+/*Find the name and grade of the student(s) with the greatest number of friends. */
+select name,grade
+from highschooler
+where id in(select id from(select id1 as id, count(id2) as friendcount	from friend	group by id1) as temp2 
+						where temp2.friendcount=(select max(friendcount) 
+												from(select id1 as id, count(id2) as friendcount from friend group by id1) as temp1));
